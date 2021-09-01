32dd52dea24faa2b9feabc15a0b07330802464df
Dgryzhkov <dgryzhkov@gmail.com>
2021-09-01 15:32:59 +0700

    add gradient

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+
+class BallPage extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.topLeft,
+            colors: <Color>[
+              Color(0xff0756f3),
+              Color(0xff7796d5)
+            ]
+          ),
+        ),
+
+      ),
+    );
+  }
+}
