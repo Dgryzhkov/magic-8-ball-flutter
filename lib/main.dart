4e9dceff66dea8d44a9ab01e69cf793f70e81b02
Dgryzhkov <dgryzhkov@gmail.com>
2021-09-01 16:34:35 +0700

    add random answer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,34 +10,57 @@ void main() => runApp(
     );
 
 
-class BallPage extends StatelessWidget {
+class BallPage extends StatefulWidget {
 
+  @override
+  _BallPageState createState() => _BallPageState();
+}
 
+class _BallPageState extends State<BallPage> {
+  int DiceNumber=1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: Text('Ask Me Anything'),
+        title: Center(child: Text('Ask Me Anything')),
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
       body: Container(
         child: Center(
-          child: Image.asset('images/ball1.png'),
+          child: TextButton(
+            child: Image.asset('images/ball$DiceNumber.png'),
+            onPressed: rollDices(),
+          ),
         ),
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.topLeft,
-            colors: <Color>[
-              Color(0xff0756f3),
-              Color(0xff7796d5)
-            ]
+              begin: Alignment.topRight,
+              end: Alignment.topLeft,
+              colors: <Color>[
+                Color(0xff0756f3),
+                Color(0xff7796d5)
+              ]
           ),
         ),
 
       ),
     );
   }
+
+  VoidCallback rollDices() {
+    return () {
+      setState(() {
+        DiceNumber = Random().nextInt(5) + 1;
+        print ('$DiceNumber');
+      });
+    };
+  }
+
 }
+
+
+
+
+
